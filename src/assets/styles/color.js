2f4fd804a0950fcf23674207fb3b832862ec9eb4
Adnan Niaz <sanixstudio@gmail.com>
2021-09-05 05:35:34 -0700

    created styling for header component

---
+++ src/assets/styles/color.js
@@ -0,0 +1,7 @@
+export const colors = {
+  primaryBgColor: "#1DA483",
+  secondaryBgColor: "#F5F5F5",
+  primaryTextColor: "#444444",
+  secondaryTextColor: "#ffffff",
+  hrColor: "#DEDEDE"
+}
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Container, Inner, LinkWrapper, NavLink, LogoText, HeartIcon, CartIcon, PillNumber } from './styles'
 
 const Header = () => {
+  const wishNumber = 4
+  const inCart = 12
+
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Container>
+      <Inner>
+        <LogoText to="/">Sani Store</LogoText>
+        <LinkWrapper>
+          <NavLink to="wishlist"><HeartIcon />
+            <PillNumber>{wishNumber}</PillNumber>
+          </NavLink>
+          <NavLink to="cart"><CartIcon />
+          <PillNumber>{inCart}</PillNumber>
+          </NavLink>
+        </LinkWrapper>
+      </Inner>
+    </Container>
   )
 }
 
