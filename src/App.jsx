107a65a42bb993d69a7f0c32523ffa70b02f51de
adnan niaz <sanixstudio@gmail.com>
2024-01-27 03:48:32 -0800

    fixing project: initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Route, Switch } from 'react-router';
-import './App.css';
-import Header from './components/Header/Header';
-import { Home, Cart, WishList, Page404, NewHome } from './pages'
+import { Route } from "react-router";
+import "./App.css";
+import Header from "./components/Header/Header";
+import NewHome from "./pages/NewHome";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Switch>
-        <Route exact path="/"><NewHome /></Route>
-        {/* <Route path="/cart"><Cart /></Route>
+      <Route exact path="/">
+        <NewHome />
+      </Route>
+      {/* <Route path="/cart"><Cart /></Route>
         <Route path="/wishlist"><WishList /></Route>
         <Route path="/*"><Page404/></Route> */}
-      </Switch>
     </div>
   );
 }
--- src/pages/NewHome.js
+++ src/pages/NewHome.js
@@ -1,40 +1,53 @@
-import React, { useState } from 'react'
-import { HomeContainer, NewCard, CardsContainer, Filter, LoadingSpinner } from '../components'
+/* eslint-disable jsx-a11y/heading-has-content */
+import React, { useState } from "react";
+import {
+  HomeContainer,
+  NewCard,
+  CardsContainer,
+  Filter,
+  LoadingSpinner,
+} from "../components";
 
 const { useQuery } = require("react-query");
 
 const API_URL = "https://fakestoreapi.com/products";
 
 async function getStudentResults() {
-  const response = await (await fetch(API_URL)).json()
-  return response
+  const response = await (await fetch(API_URL)).json();
+  return response;
 }
 
 const NewHome = () => {
-  const { isLoading, isError, error, data } = useQuery('studentResults', getStudentResults)
-  const [category, setCategory] = useState("")
+  const { isLoading, isError, error, data } = useQuery(
+    "studentResults",
+    getStudentResults
+  );
+  const [category, setCategory] = useState("");
 
   function filterByCategory(categoryName) {
-    if (categoryName) return data.filter(product => product.category === categoryName)
-    else return data
+    if (categoryName)
+      return data.filter((product) => product.category === categoryName);
+    else return data;
   }
 
   if (isLoading) {
-    return (<LoadingSpinner />)
+    return <LoadingSpinner />;
   }
 
   if (isError) {
-    return (<HomeContainer>{error}</HomeContainer>)
+    return <HomeContainer>{error}</HomeContainer>;
   }
 
   return (
     <HomeContainer>
       <Filter setCategory={setCategory} />
       <CardsContainer>
-        {filterByCategory(category).map(card => <NewCard key={card.id} product={card} />)}
+        {filterByCategory(category).map((card) => (
+          <NewCard key={card.id} product={card} />
+        ))}
       </CardsContainer>
     </HomeContainer>
-  )
-}
+  );
+};
 
-export default NewHome
+export default NewHome;
