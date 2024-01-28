deac806d0e056acbf086b40fd39577339e55e9da
Adnan Niaz <sanixstudio@gmail.com>
2024-01-28 00:31:20 -0800

    added banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,21 +1,40 @@
 import { Button } from "@radix-ui/themes";
 import React from "react";
 
+import bannerBg from "../../assets/images/ban.png";
+
 const Banner = () => {
   return (
     <div className="w-full mb-40">
       <div className="max-w-[1440px] mx-auto">
-        <div className="mx-4 border bg-slate-500">
-          <div className="flex flex-col py-8 px-16 gap-5 bg-teal-300 max-w-[460px] rounded-r-full">
-            <span className="md:text-xl">OUR EXCLUSIVE</span>
+        <div
+          className="mx-4 border bg-[#ffafcf]/10"
+          style={{
+            backgroundImage: `url(${bannerBg})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        >
+          <div className="flex flex-col py-8 px-16 text-white gap-5 bg-[#ffafcf] max-w-[460px] rounded-r-full">
+            <span style={{textShadow: "2px 2px 2px #000000"}} className="md:text-xl font-bold">OUR EXCLUSIVE</span>
             <div className="flex gap-4">
-              <span className="text-5xl md:text-7xl font-bold">15%</span>
-              <span className="text-5xl md:text-7xl font-bold">OFF</span>
+              <span
+                style={{ textShadow: "5px 5px 6px #000000" }}
+                className="text-5xl md:text-7xl font-bold"
+              >
+                15%
+              </span>
+              <span
+                style={{ textShadow: "5px 5px 6px #000000" }}
+                className="text-5xl md:text-7xl font-bold"
+              >
+                OFF
+              </span>
             </div>
-            <span className="md:text-2xl">
+            <span style={{textShadow: "2px 2px 1px #000000"}} className="md:text-2xl">
               Men Shirts, Laptops, Speakers and Fry pans
             </span>
-            <Button className="self-start">Shopt Now</Button>
+            <Button className="self-start">Shop Now</Button>
           </div>
         </div>
       </div>
